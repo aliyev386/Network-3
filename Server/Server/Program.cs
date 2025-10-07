639c3b89715450729c3f33d4b284438c34c4614d
Alıyev Omar Anar <aliye_oa18@itstep.edu.az>
2025-10-07 18:38:55 +0400

    first commit

---
+++ Server/Server/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Sockets;
+using System.Net;
+
+namespace Server;
+class Program
+{
+    public static void Main()
+    {
+        var ip = IPAddress.Parse("127.0.0.1");
+        var port = 27001;
+        TcpListener tcpListener = new TcpListener(ip, port);
+        tcpListener.Start(10);
+        Console.WriteLine($"{tcpListener.Server.LocalEndPoint} listening.....");
+        var client = tcpListener.AcceptTcpClient();
+        Console.WriteLine($"{client.Client.RemoteEndPoint} connected server....");
+        var stream = client.GetStream();
+        var br = new BinaryReader(stream);
+        var bw = new BinaryWriter(stream);
+        while (true)
+        {
+            Console.WriteLine(br.ReadString());
+            bw.Write("Good Message");
+        }
+
+
+
+    }
+}
---
+++ Server/Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+
+namespace Test;
+class Program
+{
+    public static void Main()
+    {
+        var prosesName = Process.Start("Taskmgr.exe");
+
+
+    }
+}
